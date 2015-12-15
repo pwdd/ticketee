d86419f7f301a05d00ec04bee389201cd4ecf4c3
pwdd <aina@pintowebdevelopment.com>
2015-12-14 19:58:45 -0800

    only managers and admins can delete tickets

--- spec/policies/ticket_policy_spec.rb
+++ spec/policies/ticket_policy_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe TicketPolicy do
       it { should_not permit_action :show }
       it { should_not permit_action :create }
       it { should_not permit_action :update }
+      it { should_not permit_action :destroy }
     end
 
     context 'for viewers of the project' do
@@ -22,6 +23,7 @@ RSpec.describe TicketPolicy do
       it { should permit_action :show }
       it { should_not permit_action :create }
       it { should_not permit_action :update }
+      it { should_not permit_action :destroy }
     end
 
     context 'for editors of the project' do
@@ -30,6 +32,7 @@ RSpec.describe TicketPolicy do
       it { should permit_action :show }
       it { should permit_action :create }
       it { should_not permit_action :update }
+      it { should_not permit_action :destroy }
 
       context 'when the editor created the ticket' do
         before { ticket.author = user }
@@ -44,9 +47,10 @@ RSpec.describe TicketPolicy do
       it { should permit_action :show }
       it { should permit_action :create }
       it { should permit_action :update }
+      it { should permit_action :destroy }
     end
 
-    context 'form managers of other projects' do
+    context 'for managers of other projects' do
       before do
         assign_role!(user, :manager, FactoryGirl.create(:project))
       end
@@ -54,6 +58,7 @@ RSpec.describe TicketPolicy do
       it { should_not permit_action :show }
       it { should_not permit_action :create }
       it { should_not permit_action :update }
+      it { should_not permit_action :destroy }
     end
 
     context 'for administrators' do
@@ -62,6 +67,7 @@ RSpec.describe TicketPolicy do
       it { should permit_action :show }
       it { should permit_action :create }
       it { should permit_action :update }
+      it { should permit_action :destroy }
     end
   end
 end
