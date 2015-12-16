6302308616f952c1de6444b307be6402a01a5a94
pwdd <aina@pintowebdevelopment.com>
2015-12-16 00:32:38 -0800

    managing_roles is not done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :roles
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
@@ -20,5 +19,9 @@ class User < ActiveRecord::Base
 
   def inactive_message
     archived_at.nil? ? super : :archived
+  end
+
+  def role_on(project)
+    roles.find_by(project_id: project).try(:name)
   end
 end
