76b8475f2425454bc3cf15f3ec8a360a0bfe8bea
pwdd <aina@pintowebdevelopment.com>
2016-01-31 22:40:15 -0800

    authorized users can now leave comments on tickets

--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -15,7 +15,7 @@ RSpec.feature 'Users can only see the appropriate links' do
     end
   end
 
-  context '"non-admin users (project viewers)' do
+  context 'non-admin users (project viewers)' do
     before do
       login_as(user)
       assign_role!(user, :viewer, project)
@@ -50,6 +50,11 @@ RSpec.feature 'Users can only see the appropriate links' do
       visit project_ticket_path(project, ticket)
       expect(page).not_to have_link 'Delete Ticket'
     end
+
+    scenario 'cannot see the New Comment form' do
+      visit project_ticket_path(project, ticket)
+      expect(page).not_to have_heading 'New Comment'
+    end
   end
 
   context 'admin users' do
@@ -83,6 +88,11 @@ RSpec.feature 'Users can only see the appropriate links' do
     scenario 'can see the Delete Ticket link' do
       visit project_ticket_path(project, ticket)
       expect(page).to have_link 'Delete Ticket'
+    end
+
+    scenario 'can see the New Comment form' do
+      visit project_ticket_path(project, ticket)
+      expect(page).to have_heading 'New Comment'
     end
   end
 end
