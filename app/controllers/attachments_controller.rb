29b20b76740ac1522ca6799f8cab835b26005da8
pwdd <ainapinto@gmail.com>
2016-04-04 01:29:03 -0700

    files are served correctly from s3 in production

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -11,6 +11,20 @@ class AttachmentsController < ApplicationController
   def show
     attachment = Attachment.find(params[:id])
     authorize attachment, :show?
-    send_file attachment.file.path, disposition: :inline
+    send_file file_to_send(attachment), disposition: :inline
+  end
+
+  private
+
+  def file_to_send(attachment)
+    if URI.parse(attachment.file.url).scheme
+      filename = "/tmp/#{attachment.attributes["file"]}"
+      File.open(filename, 'wb+') do |tf|
+        tf.write open(attachment.file.url).read
+      end
+      filename
+    else
+      attachment.file.path
+    end
   end
 end
